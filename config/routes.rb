de464458e1f233447b304ae9f75c0145ce83e900
github username <github email address>
2020-02-24 09:54:47 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+  # resources :songs, only: [:index]
+  # post 'songs/upload', to: 'songs#upload'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+# Rails.application.routes.draw do
+#   resources :customers, only: [:index]
+#
+#   # The below line is added.
+#   post 'customers/upload', to: 'customers#upload'
+#   # The above line is added.
+# end
